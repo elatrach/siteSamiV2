493f08df1ceff30ecb49830b81813a97cecb6588
unknown <youssef.elatrach83@gmail.com>
2021-03-31 15:40:38 +0200

    init

---
+++ portfolio/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet" >
+    <link href="style.css" rel="stylesheet">
+    <title>Document</title>
+
+
+
+</head>
+<body> 
+    <div class="jumbotron jumbotron-fluid banner py-0 my-0 d-flex flex-column  justify-content-start" style="height: 700px;" >
+            <nav class="navbar navbar-dark  navbar-expand-lg  font-weight-bold " >
+                <a class="navbar-brand active" href="#">Accueil</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarNavDropdown"> 
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Presentation <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item mr-lg-1">
+                      <a class="nav-link" href="#">Réalisation</a>
+                    </li>
+                    <li class="nav-item mr-lg-1">
+                      <a class="nav-link" href="#">Activités</a>
+                    </li>
+                    <li class="nav-item mr-lg-1">
+                        <a class="nav-link" href="#">Compétence</a>
+                    </li>
+                    <li class="nav-item mr-lg-1">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mon Cv</a>
+                    </li>
+                  </ul>
+                </div>
+            </nav>
+     
+      <div class="align-self-center my-auto font-weight-bold text-white"> 
+          <p>Bonjour</p>
+          <h1>Je suis youssef El atrach </h1>
+          <p>Développeur web </p>
+      </div>
+    </div>
+    <div class=" jumbotron jumbotron-fluid py-5 my-0 d-flex align-items-center" style="background-color: azure;">
+      <div class="container ">
+        <div class="row">
+          <div class="col-lg-7 mx-auto">
+            <h3 class="text-center">Présentation</h3>
+            <hr>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quia tenetur accusantium similique, sit ipsam voluptas sint temporibus nihil laboriosam, veniam culpa perferendis maxime blanditiis deleniti eos nostrum nesciunt! Aperiam.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class=" jumbotron jumbotron-fluid py-5 my-0 d-flex align-items-center" style="background-color: azure;">
+    
+    </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="../js/bootstrap.min.js" ></script>
+</body>
+    
+</body>
+</html>
